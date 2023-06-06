763bb6c84291bada101fa9dc743b6ee8eaee3131
Connor Meehan <18220073+cmeehan1991@users.noreply.github.com>
2023-06-06 10:46:07 -0400

    Initial commit

---
+++ store-locator.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Plugin Name: Store Locator 
+ * Plugin URI: 
+ * Version: 0.0.1 
+ * Author: CBM Web Development
+ * License: GPLv2 
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: store_locator 
+ */
+ 
+ define( 'STORE_LOCATOR_URI', plugin_dir_url( __FILE__ ));
+ define( 'STORE_LOCATOR_PATH', plugin_dir_path( __FILE__ ));
+ deinfe( 'SITE_URL', get_site_url() );
+ define( 'STORE_LOCATOR_TEXTDOMAIN', 'store_locator');
+ define( 'STORE_LOCATOR_VERSION', '0.0.1');
+ 
+ 
