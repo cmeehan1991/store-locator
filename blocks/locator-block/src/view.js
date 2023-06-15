52a3bd4937dd2efd44768e58dd53e33885cd25be
Connor Meehan <18220073+cmeehan1991@users.noreply.github.com>
2023-06-15 10:28:09 -0400

    Removing key

--- blocks/locator-block/src/view.js
+++ blocks/locator-block/src/view.js
@@ -3,7 +3,6 @@ import apiFetch from '@wordpress/api-fetch';
 import List from 'list.js';
 
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-	//key: "AIzaSyDld488a9g_-3n1q9Z9ZI43Ijb_6H-U2rk",
 	key: $('.store-locator').data('key'),
 	v: "weekly",
 	// Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
